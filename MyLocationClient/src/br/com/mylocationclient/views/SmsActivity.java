8c9338dfb12987d74cbbec90fc5eecd1bc31fc2c
robertoconceicao <conceicao.roberto@gmail.com>
2014-07-22 22:41:28 -0300

    Adicionando controle de status de envio do sms, sucesso ou falhas

--- MyLocationClient/src/br/com/mylocationclient/views/SmsActivity.java
+++ MyLocationClient/src/br/com/mylocationclient/views/SmsActivity.java
@@ -1,6 +1,11 @@
 package br.com.mylocationclient.views;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.telephony.gsm.SmsManager;
 import android.widget.Toast;
@@ -14,13 +19,13 @@ import br.com.mylocationclient.core.ObserverHost;
 
 public class SmsActivity extends Activity implements ObserverHost {
 
-	private Client client; 
-	
+	private Client client;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sms);
-		
+
 		client = Client.getInstance();
 		client.setObserverHost(this);
 	}
@@ -40,38 +45,76 @@ public class SmsActivity extends Activity implements ObserverHost {
 	 */
 	@Override
 	public void onMessage(Message message) {
-		switch(message.getType()) {
-			case GlobalDefines.TYPE_COMMAND:
-				onCommand((Command) message);				
+		switch (message.getType()) {
+		case GlobalDefines.TYPE_COMMAND:
+			onCommand((Command) message);
 			break;
-			default:
-				dialog("Comando Inválido");			
-		}
-	}
-	
-	private void onCommand(Command command) {
-		switch(command.getOperation()){
-		case GlobalDefines.OPERATION_SMS_SEND:
-			sendSms((PacoteSms)command.getData());
+		default:
+			dialog("Comando Inválido");
 		}
 	}
 
-	public void sendSms(PacoteSms pacoteSms){
+	private void onCommand(Command command) {
+		switch (command.getOperation()) {
+		case GlobalDefines.OPERATION_SMS_SEND:
+			sendSms(command);
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public void sendSms(Command command) {
 		dialog("Enviando sms");
 		
+		String SENT = "SMS_SENT";	
+		Intent intentSent = new Intent(SENT);		
+		intentSent.putExtra("command", command);
+		
+		PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,intentSent, 0);
+	
+		//—when the SMS has been sent—
+		registerReceiver(new BroadcastReceiver() {			
+				@Override
+				public void onReceive(Context context, Intent intent) {
+					Command command = (Command) intent.getExtras().getSerializable("command");
+					
+					switch (getResultCode()){
+					case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "SMS sent "+command.getRid(), 
+					Toast.LENGTH_SHORT).show();
+					break;
+					case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+					Toast.makeText(getBaseContext(), "Generic failure "+command.getRid(), 
+					Toast.LENGTH_SHORT).show();
+					break;
+					case SmsManager.RESULT_ERROR_NO_SERVICE:
+					Toast.makeText(getBaseContext(), "No service "+command.getRid(), 
+					Toast.LENGTH_SHORT).show();
+					break;
+					case SmsManager.RESULT_ERROR_NULL_PDU:
+					Toast.makeText(getBaseContext(), "Null PDU "+command.getRid(), 
+					Toast.LENGTH_SHORT).show();
+					break;
+					case SmsManager.RESULT_ERROR_RADIO_OFF:
+					Toast.makeText(getBaseContext(), "Radio off "+command.getRid(), 
+					Toast.LENGTH_SHORT).show();
+					break;
+				}
+			}
+		}, new IntentFilter(SENT));
+		
+		PacoteSms pacoteSms = (PacoteSms) command.getData();
 		SmsManager smsManager = SmsManager.getDefault();
-		smsManager.sendTextMessage(pacoteSms.getPhone(), 
-								null, 
-								pacoteSms.getSms(), null, null);
+		smsManager.sendTextMessage(pacoteSms.getPhone(), null, pacoteSms.getSms(), sentPI, null);
 	}
 	
 	public void dialog(final String message) {
 		Runnable run = new Runnable() {
 			@Override
-			public void run() {				
-				Toast.makeText(SmsActivity.this, message, Toast.LENGTH_SHORT).show();		
+			public void run() {
+				Toast.makeText(SmsActivity.this, message, Toast.LENGTH_SHORT)
+						.show();
 			}
-		};		
-		runOnUiThread(run);		
+		};
+		runOnUiThread(run);
 	}
 }
